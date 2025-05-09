7c1a4b2340cee321caf2ac87c2f975a01df67bf6
Wesley <wesleyadwebster@gmail.com>
2025-05-09 01:27:01 -0500

    Mobile responsive temp styling for now

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 import { navigationHeader } from '@/config';
+import MobileNav from './MobileNavHeader/MobileNav';
 import classes from './Header.module.scss';
 
-const { siteHeader } = classes;
+const { siteHeader, desktopNav, mobileNavToggle, hamburger, open } = classes;
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <header className={siteHeader}>
@@ -13,17 +22,31 @@ const Header = () => {
                     CollaboNote
                 </Link>
             </h1>
-            <nav>
-                {navigationHeader.map((nav) => {
-                    return (
-                        <Link
-                            key={nav.navigation}
-                            href={nav.navigation}>
-                            {nav.navigation}
-                        </Link>
-                    )
-                })}
+            
+            <nav className={desktopNav}>
+                {navigationHeader.map((nav) => (
+                    <Link
+                        key={nav.navigation}
+                        href={nav.path}>
+                        {nav.navigation}
+                    </Link>
+                ))}
             </nav>
+            
+            <button
+                className={mobileNavToggle}
+                onClick={toggleMenu}
+                aria-label="Toggle navigation menu"
+                aria-expanded={isMenuOpen}
+            >
+                <div className={`${hamburger} ${isMenuOpen ? open : ''}`}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            
+            <MobileNav isOpen={isMenuOpen} toggleMenu={toggleMenu} />
         </header>
     );
 };
