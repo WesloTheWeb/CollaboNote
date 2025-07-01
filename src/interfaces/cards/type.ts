8d526f005830ef6dcae9aec700423bb38b70f938
Wesley Webster <wesleyadwebster@gmail.com>
2025-07-01 03:37:53 -0500

    type correction goalId

--- src/interfaces/cards/type.ts
+++ src/interfaces/cards/type.ts
@@ -1,7 +1,7 @@
 type goalStatuses = 'Completed' | 'On-going';
 
 export interface goalCard {
-    goalId: string;
+    goalId?: string;
     goalName: string,
     goalDescription: string,
     goalStatus: goalStatuses,
