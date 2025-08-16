4eb083a76573747260e4bb60d05bb31b20845b6f
Wesley Webster <wesley@moonnox.com>
2025-08-16 11:27:55 -0500

    health check endpoint

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -3,7 +3,7 @@ import db from '@/lib/db';
 
 export async function GET() {
   try {
-    const isConnected = await db.testConnection();
+    // const isConnected = await db.testConnection();
     // const isConnected = false
     
     if (isConnected) {
